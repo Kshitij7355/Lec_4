8c4571c2ee475bbeb8678b4c61c2867a46ef8f3f
Kshitij Singh <114972491+Kshitij7355@users.noreply.github.com>
2024-08-13 15:39:54 +0530

    Add files via upload

---
+++ Data_Tpes_2.java
@@ -0,0 +1,18 @@
+package Lec5;
+
+import java.util.Scanner;
+
+public class Data_Tpes_2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long l =1000000000l;
+		int i =390;
+		byte b = (byte)i;
+		System.out.println(b);
+		l=s.nextLong();
+		System.out.println(l);
+		System.out.println(i);
+	}
+
+}
